e8795547467a0fd3b9075d6ece31ad6058a64eb1
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-06 17:25:00 +0530

    refactor: fix checkbox styling

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -2,9 +2,10 @@ import "../App.css";
 
 export default function Checkbox({ name }: { name: string }) {
   return (
-    <label className="label">
+    <div className="block">
       <span>{name}</span>
-      <input type="checkbox" />
-    </label>
+      <input id={name} type="checkbox" />
+      <label htmlFor={name}></label>
+    </div>
   );
 }
